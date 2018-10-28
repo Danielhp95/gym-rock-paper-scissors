cd6dcd47722f1552be05309252d5b1914a216123
Daniel Hernandez <danielhp95@gmail.com>
2018-10-28 21:25:41 +0000

    explicitly adding package

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='gym_rock_paper_scissors',
-      version='0.0.1',
+      version='0.0.121',
       description='OpenAI gym environment for a repeated game of Rock-Paper-Scissors',
       url='https://github.com/Danielhp95/gym-rock-paper-scissors',
       author='Sarios',
       author_email='rockpapersass@xcape.com',
+      packages=['gym_rock_paper_scissors'],
       install_requires=['gym']
       )
