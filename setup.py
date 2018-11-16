69ca33f61e3c521819105e8c8e48882e6d26ce62
Daniel Hernandez <danielhp95@gmail.com>
2018-11-16 16:19:32 +0000

    moved fixed agents inside rock-paper-scissors sirectory

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_rock_paper_scissors',
-      version='0.0.123',
+      version='0.0.125',
       description='OpenAI gym environment for a repeated game of Rock-Paper-Scissors',
       url='https://github.com/Danielhp95/gym-rock-paper-scissors',
       author='Sarios',
       author_email='rockpapersass@xcape.com',
-      packages=['gym_rock_paper_scissors', 'fixed_agents'],
+      packages=find_packages(),
       install_requires=['gym']
       )
