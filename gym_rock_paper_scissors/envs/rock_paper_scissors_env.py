537bbfca769564cda917a814fd9fb58c2fbbc8fb
Kevin Denamganai <denamganai.kevin@gmail.com>
2019-01-15 16:11:43 +0000

    revert previous one-hot-encoded, for adaptation has been done elsewhere... added interface methods to MixedStrategyAgent.

--- gym_rock_paper_scissors/envs/rock_paper_scissors_env.py
+++ gym_rock_paper_scissors/envs/rock_paper_scissors_env.py
@@ -1,13 +1,13 @@
 from enum import Enum
 import gym
 from gym.spaces import Discrete, Tuple
-import numpy as np
 
 
 class Action(Enum):
     ROCK     = 0
     PAPER    = 1
     SCISSORS = 2
+
 
 class RockPaperScissorsEnv(gym.Env):
     '''
@@ -17,7 +17,7 @@ class RockPaperScissorsEnv(gym.Env):
     Reward function: -1 for losing, +1 for wining.
     '''
 
-    def __init__(self, stacked_observations=3, max_repetitions=100000):
+    def __init__(self, stacked_observations=3, max_repetitions=10):
         '''
         :param stacked_observations: Number of action pairs to be considered as part of the state
         :param max_repetitions: Number of times the game will be played
@@ -30,8 +30,6 @@ class RockPaperScissorsEnv(gym.Env):
         self.state = [None for _ in range(stacked_observations)]
         self.action_space_size = len([a for a in Action])
         self.state_space_size = self.calculate_state_space_size(stacked_observations, self.action_space_size)
-        self.one_hot_state = np.zeros(self.state_space_size)
-        self.one_hot_state[0] = 1.0
 
         self.repetition = 0
         self.max_repetitions = max_repetitions
@@ -59,9 +57,7 @@ class RockPaperScissorsEnv(gym.Env):
         filetered_state = filter(lambda x: x is not None, state)
         flattened_ternary_state = [action.value for joint_action in filetered_state for action in joint_action]
         decimal_from_ternary = sum([number_of_actions**i * value for i, value in enumerate(flattened_ternary_state[::-1])])
-        self.one_hot_state = np.zeros(self.state_space_size)
-        self.one_hot_state[decimal_from_ternary+offset] = 1.0
-        return self.one_hot_state
+        return decimal_from_ternary + offset
 
     def calculate_hash_offset(self, state, number_of_actions):
         """
